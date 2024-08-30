0596354ea5f366c0f3b26eb8c1f626cba0d3c0bd
Karl Pierce <kmp5@vt.edu>
2024-08-30 12:17:13 -0400

    Working on a fit check algorithm (this will make it possible to approximate the fit of even a tensor network)

--- src/lib/ITensorCPD/converge_checks.jl
+++ src/lib/ITensorCPD/converge_checks.jl
@@ -1,3 +1,4 @@
+using ITensors: hadamard_product!
 abstract type ConvergeAlg end
 
 mutable struct NoCheck <: ConvergeAlg
@@ -20,17 +21,54 @@ function save_mttkrp(::ConvergeAlg, ::ITensor)
 end
 
 mutable struct FitCheck <: ConvergeAlg
+  iter::Int
   counter::Int
+  tolerance::Number
   max_counter::Int
   ref_norm::Number
   MttKRP::ITensor
   fit::Number
+  final_fit::Number 
 
-  FitCheck(max, norm) = new(0, max, norm, ITensor(), 1)
+  FitCheck(tol, max, norm) = new(0, 0, tol, max, norm, ITensor(), 1, 0)
 end
 
 save_mttkrp(fit::FitCheck, mttkrp::ITensor) = fit.MttKRP = mttkrp
 
-function check_converge(check::FitCheck, cp::CPD, partial_gram)
+function check_converge(check::FitCheck, cp::CPD, partial_gram; verbose = true)
+  check.iter += 1
+  rank = ind(cp.λ, 1)
+  # inner_prod = (check.MttKRP * conj(prime(cp.factors[end]; tags=tags(rank))) * (cp.λ * δ(rank')))[]
+  inner_prod = sum(hadamard_product(check.MttKRP, cp.factors[end]) * cp.λ)
+  fact_square = norm_factors(partial_gram, cp.λ)
+  normResidual = sqrt(abs(check.ref_norm * check.ref_norm + fact_square - 2 * abs(inner_prod)));
+  curr_fit = 1. - (normResidual / check.ref_norm);
+  Δfit = abs(check.fit - curr_fit)
+  check.fit = curr_fit
 
+  if (verbose) 
+    println("$(dim(rank))\t $(check.iter) \t $(curr_fit) \t $(Δfit)")
+  end
+
+  if Δfit < check.tolerance
+    check.counter += 1
+    if check.counter >= 2
+      check.iter = 0
+      check.counter = 0
+      check.final_fit = check.fit
+      check.fit = 0
+      return true
+    end
+  else
+    check.counter = 0
+  end
+  return false
+end
+
+function norm_factors(partial_gram::Vector, λ::ITensor)
+  had = copy(partial_gram[1])
+  for i in 2:length(partial_gram)
+    hadamard_product!(had, had, partial_gram[i])
+  end
+  return (had * (λ * prime(λ)))[]
 end
