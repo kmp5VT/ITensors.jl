984f6101d227bb1d0067445eb7bbdd50e44c13f1
Karl Pierce <kmp5@vt.edu>
2024-08-30 10:00:23 -0400

    Move included

--- src/lib/ITensorCPD/ITensorCPD.jl
+++ src/lib/ITensorCPD/ITensorCPD.jl
@@ -2,7 +2,7 @@ module ITensorCPD
 include("row_norm.jl")
 include("algorithms.jl")
 include("cpd.jl")
+include("converge_checks.jl")
 include("optimizers.jl")
 include("reconstruct.jl")
-include("converge_checks.jl")
 end
