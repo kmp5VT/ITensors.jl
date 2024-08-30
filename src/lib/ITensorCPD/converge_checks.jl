d6ee70bc1565f24930c2eb6071f369178420e1c5
Karl Pierce <kmp5@vt.edu>
2024-08-30 09:59:09 -0400

    Working on smarter convergence

--- src/lib/ITensorCPD/converge_checks.jl
+++ src/lib/ITensorCPD/converge_checks.jl
@@ -1,3 +1,36 @@
-function check_converge()
+abstract type ConvergeAlg end
+
+mutable struct NoCheck <: ConvergeAlg
+  counter::Int
+  max_counter::Int
+  fit::Number
+
+  NoCheck(max) = new(0, max, -1)
+end
+
+function check_converge(check::NoCheck, ::CPD, partial_gram)
+  if check.counter ≥ check.max_counter
+    return true
+  end
+  check.counter += 1
   return false
+end
+
+function save_mttkrp(::ConvergeAlg, ::ITensor)
+end
+
+mutable struct FitCheck <: ConvergeAlg
+  counter::Int
+  max_counter::Int
+  ref_norm::Number
+  MttKRP::ITensor
+  fit::Number
+
+  FitCheck(max, norm) = new(0, max, norm, ITensor(), 1)
+end
+
+save_mttkrp(fit::FitCheck, mttkrp::ITensor) = fit.MttKRP = mttkrp
+
+function check_converge(check::FitCheck, cp::CPD, partial_gram)
+
 end
--- src/lib/ITensorCPD/optimizers.jl
+++ src/lib/ITensorCPD/optimizers.jl
@@ -1,15 +1,17 @@
-function als_optimize(cp::CPD, rank::Index; maxiters = nothing)
-  iter = 0
-  maxiters = isnothing(maxiters) ? 1 : maxiters
+function als_optimize(cp::CPD, rank::Index; maxiters = 1, kwargs...)
+  return als_optimize(cp, rank, NoCheck(maxiters); kwargs...)
+end
+
+function als_optimize(cp::CPD, rank::Index, converge::ConvergeAlg;)
   part_grammian = cp.factors .* conj(prime.(cp.factors; tags=tags(rank)))
   num_factors = length(cp.factors)
   @show norm(cp.target - reconstruct(cp)) / norm(cp.target)
   λ = copy(cp.λ)
   factors = copy(cp.factors)
-  while iter < maxiters
+  while !check_converge(converge, cp, part_grammian)
     for fact in 1:num_factors
       ## compute the matrized tensor time khatri rao product with a provided algorithm.
-      m = mttkrp(cp.mttkrp_alg, factors, cp.target, rank, fact)
+      m = mttkrp(cp.mttkrp_alg, factors, cp, rank, fact)
       ## compute the grammian which requires the hadamard product
       grammian = similar(part_grammian[1])
       fill!(grammian, one(eltype(cp)))
@@ -20,14 +22,14 @@ function als_optimize(cp::CPD, rank::Index; maxiters = nothing)
         grammian = hadamard_product(grammian, part_grammian[i])
       end
       # potentially save the MTTKRP for the loss function
+
+      ## potentially better to first inverse the grammian then contract
       factors[fact], λ = row_norm(itensor(array(grammian) \ array(m), inds(m)), ind(m, 2))
       part_grammian[fact] = factors[fact] * conj(prime(factors[fact]; tags=tags(rank)))
     end
-    if check_converge()
-      break
-    end
-    iter += 1
+
   end
+
   @show norm(cp.target - reconstruct(factors, λ)) / norm(cp.target)
-  return CPD(cp.target, factors, λ, cp.mttkrp_alg)
+  return CPD(cp.target, factors, λ, cp.mttkrp_alg, cp.additional_items)
 end
