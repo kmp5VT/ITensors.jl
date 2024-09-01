34dffce93a0f223d7d2e5648737f1c522725b055
Karl Pierce <kmp5@vt.edu>
2024-09-01 16:09:09 -0400

    small fix

--- src/lib/ITensorCPD/test/runtests.jl
+++ src/lib/ITensorCPD/test/runtests.jl
@@ -1,7 +1,7 @@
 using Test
 include("$(@__DIR__)/../ITensorCPD.jl")
 using .ITensorCPD: als_optimize, direct, random_CPD, random_CPD_square_network, row_norm, reconstruct
-using ITensors: Index, ITensor, array, dim, norm, random_itensor
+using ITensors: Index, ITensor, array, contract, dim, norm, random_itensor
 
 @testset "Norm Row test, elt=$elt" for elt in [Float32, Float64, ComplexF32, ComplexF64]
   i,j = Index.((20,30))
@@ -56,39 +56,32 @@ end
 
 ## Complex optimization still does not work
 @testset "Standard CPD, elt=$elt" for elt in [Float32, Float64]
-## Working here
-i,j,k = Index.((20,30,40))
-r = Index(400, "CP_rank")
-A = random_itensor(elt, i,j,k);
-cp_A = random_CPD(A, r);
+  ## Working here
+  i,j,k = Index.((20,30,40))
+  r = Index(400, "CP_rank")
+  A = random_itensor(elt, i,j,k);
+  cp_A = random_CPD(A, r);
 
-opt_A = als_optimize(cp_A, r; maxiters=100);
-@test norm(reconstruct(opt_A) - A) / norm(A) < 1e-7
+  opt_A = als_optimize(cp_A, r; maxiters=100);
+  @test norm(reconstruct(opt_A) - A) / norm(A) < 1e-7
 
-check = ITensorCPD.FitCheck(1e-15, 100, norm(A))
-opt_A = als_optimize(cp_A, r, check);
-@test norm(reconstruct(opt_A) - A) / norm(A) ≤ 1.0 - ITensorCPD.fit(check)
+  check = ITensorCPD.FitCheck(1e-15, 100, norm(A))
+  opt_A = als_optimize(cp_A, r, check);
+  @test norm(reconstruct(opt_A) - A) / norm(A) ≤ 1.0 - ITensorCPD.fit(check)
 
-cp_A = random_CPD(A, r; algorithm=direct());
-opt_A = als_optimize(cp_A, r; maxiters=100);
-@test norm(reconstruct(opt_A) - A) / norm(A) < 1e-7
+  cp_A = random_CPD(A, r; algorithm=direct());
+  opt_A = als_optimize(cp_A, r; maxiters=100);
+  @test norm(reconstruct(opt_A) - A) / norm(A) < 1e-7
 
-check = ITensorCPD.FitCheck(1e-15, 100, norm(A))
-cp_A = random_CPD(A, r; algorithm=direct());
-opt_A = als_optimize(cp_A, r, check);
-@test norm(reconstruct(opt_A) - A) / norm(A) ≤ 1.0 - ITensorCPD.fit(check)
+  check = ITensorCPD.FitCheck(1e-15, 100, norm(A))
+  cp_A = random_CPD(A, r; algorithm=direct());
+  opt_A = als_optimize(cp_A, r, check);
+  @test norm(reconstruct(opt_A) - A) / norm(A) ≤ 1.0 - ITensorCPD.fit(check)
 end
 
 @testset "Lattice CPD, elt=$elt" for elt in [Float32, Float64]
   a,b,c,d,e,f,g,h = Index.((5,5,5,5,5,5,5,5), ("a","b","c","d","e","f","g","h"));
   w,x,y,z = Index.((5,5,5,5), ("w","x","y","z"));
-  square = [random_itensor(elt, a,b,x,w), random_itensor(elt, c,d,x,z), random_itensor(elt, g,h,z,y), random_itensor(elt, e,f,y,w)];
-  r = Index(2000,"CP_rank")
-  CP = random_CPD_square_network(square, r);
-  ## TODO write better code to take norm of square lattice
-  check = ITensorCPD.FitCheck(1e-3, 30, sqrt((contract(square) * contract(square))[]))
-  opt_A = ITensorCPD.als_optimize(CP, r, check);
-  norm(ITensorCPD.reconstruct(opt_A) - contract(square)) / sqrt((contract(square) * contract(square))[]) ≈ 1.0 - ITensorCPD.fit(check)
 
   line = [random_itensor(elt, a,b,x), random_itensor(elt, c,d,x)];
   r = Index(40,"CP_rank")
@@ -96,5 +89,14 @@ end
   check = ITensorCPD.FitCheck(1e-10, 100, sqrt((contract(line) * contract(line))[]))
 
   opt_A = ITensorCPD.als_optimize(CP, r, check);
-  norm(ITensorCPD.reconstruct(opt_A) - contract(line)) / norm(line) ≤ ITensorCPD.fit(check)
+  @test norm(ITensorCPD.reconstruct(opt_A) - contract(line)) / norm(line) ≤ ITensorCPD.fit(check)
+
+
+  square = [random_itensor(elt, a,b,x,w), random_itensor(elt, c,d,x,z), random_itensor(elt, g,h,z,y), random_itensor(elt, e,f,y,w)];
+  r = Index(2000,"CP_rank")
+  CP = random_CPD_square_network(square, r);
+  ## TODO write better code to take norm of square lattice
+  check = ITensorCPD.FitCheck(1e-3, 30, sqrt((contract(square) * contract(square))[]))
+  opt_A = ITensorCPD.als_optimize(CP, r, check);
+  @test norm(ITensorCPD.reconstruct(opt_A) - contract(square)) / sqrt((contract(square) * contract(square))[]) ≤ 1.0 - ITensorCPD.fit(check)
 end
