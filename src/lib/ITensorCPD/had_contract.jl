6c3e3dc9a681780e82df1316fd617c351e7f3c5d
Karl Pierce <kmp5@vt.edu>
2024-10-02 16:13:50 -0400

    Fix had_contract

--- src/lib/ITensorCPD/had_contract.jl
+++ src/lib/ITensorCPD/had_contract.jl
@@ -1,7 +1,7 @@
 using ITensors: ITensor, Index
-function had_contract(A::ITensor, B::ITensor, had::Index; α = true, β = false)
+function had_contract(A::ITensor, B::ITensor, had::Index; α = true)
   if had ∉ commoninds(A, B)
-    return A * B
+    return α .* A * B
   else
     position_of_had_A = findall(x -> x==had, inds(A))[]
     position_of_had_B = findall(x -> x==had, inds(B))[]
@@ -10,14 +10,15 @@ function had_contract(A::ITensor, B::ITensor, had::Index; α = true, β = false)
 
     @assert length(slices_A) == length(slices_B)
     inds_c = noncommoninds(A,B)
-    C = ITensor(promote_type(eltype(A), eltype(B)), vcat(had, inds_c...))
+    elt = promote_type(eltype(A), eltype(B))
+    C = ITensor(elt, vcat(had, inds_c...))
     ## Right now I have to fill C with zeros because I hate empty tensor
-    fill!(C, zero(eltype(C)))
+    fill!(C, zero(elt))
     slices_C = eachslice(array(C); dims=1)
     for i in 1:length(slices_A)
       a_inds = [ind(A, x) for x in 1:ndims(A) if x != position_of_had_A]
       b_inds = [ind(B, x) for x in 1:ndims(B) if x != position_of_had_B]
-      ITensors.contract!(itensor(slices_C[i], inds_c), itensor(slices_A[i], a_inds),  itensor(slices_B[i], b_inds), α, β)
+      ITensors.contract!(itensor(slices_C[i], inds_c), itensor(slices_A[i], a_inds),  itensor(slices_B[i], b_inds), elt(α))
     end
     return C
   end
