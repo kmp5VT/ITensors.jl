d93c9a8f2bc5101ba7b3ef5b227cb7014d523693
Karl Pierce <kmp5@vt.edu>
2024-10-11 11:16:46 -0400

    Start workign on potts

--- src/lib/ITensorCPD/experiments/2d-lattice-experiment.jl
+++ src/lib/ITensorCPD/experiments/2d-lattice-experiment.jl
@@ -50,6 +50,41 @@ function ising_network(
   return tn
 end
 
+function potts_network(
+  eltype::Type, s::IndsNetwork, beta::Number; h::Number=0.0, szverts=nothing
+)
+  s = insert_linkinds(s; link_space=3)
+  # s = insert_missing_internal_inds(s, edges(s); internal_inds_space=2)
+  tn = delta_network(eltype, s)
+  if (szverts != nothing)
+    for v in szverts
+      tn[v] = diagITensor(eltype[1, -1], inds(tn[v]))
+    end
+  end
+  # for edge in edges(tn)
+  #   v1 = src(edge)
+  #   v2 = dst(edge)
+  #   i = commoninds(tn[v1], tn[v2])[1]
+  #   deg_v1 = degree(tn, v1)
+  #   deg_v2 = degree(tn, v2)
+  #   f11 = exp(beta * (1 + h / deg_v1 + h / deg_v2))
+  #   f12 = exp(beta * (-1 + h / deg_v1 - h / deg_v2))
+  #   f21 = exp(beta * (-1 - h / deg_v1 + h / deg_v2))
+  #   f22 = exp(beta * (1 - h / deg_v1 - h / deg_v2))
+  #   q = eltype[f11 f12; f21 f22]
+  #   w, V = eigen(q)
+  #   w = map(sqrt, w)
+  #   sqrt_q = V * ITensors.Diagonal(w) * inv(V)
+  #   t = itensor(sqrt_q, i, i')
+  #   tn[v1] = tn[v1] * t
+  #   tn[v1] = noprime!(tn[v1])
+  #   t = itensor(sqrt_q, i', i)
+  #   tn[v2] = tn[v2] * t
+  #   tn[v2] = noprime!(tn[v2])
+  # end
+  return tn
+end
+ptn = potts_network(Float64, s, 1.0)
 ## This function primes the internal indices of a 2d
 ## network. Doing so allows one to compute the norm
 ## of the ising network more easily
@@ -77,14 +112,14 @@ function replace_inner_w_prime_loop(tn)
 end
 
 ##############################################
-nx = 7
-ny = 8
+nx = 5
+ny = 5
 
 beta = 1.0
 h = 0.0
 
 h = 0
-s = IndsNetwork(named_grid((nx, ny)); link_space=2);
+s = IndsNetwork(named_grid((nx, ny)); link_space=3);
 
 function contract_loops(r1, r2, tn, i; check_svd::Bool=false, old_contract = true)
   s1 = subgraph(
